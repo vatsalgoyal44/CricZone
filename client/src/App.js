bac13ce46206ac6ec91bbd611d4d47e599ae3dc6
Vatsal <vatsalgoyal44@gmail.com>
2023-04-06 18:39:13 +0530

    UI changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import Homepage from './Components/pages/home'
+import Sidebar from './Components/sidebar/sidebar';
+import Navbar from './Components/NavigationBar/navbar';
+import Teampage from './Components/pages/team';
+import { BrowserRouter, Routes, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+      <Navbar />
+
+        <Routes>
+        <Route path="/" element={<Homepage />}/>
+          <Route exact path="/home" element={<Homepage />}/>
+          <Route exact path="/teams/:team" element={<Teampage />}/>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
