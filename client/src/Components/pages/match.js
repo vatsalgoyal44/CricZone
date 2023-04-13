e3394eed4420f1fe91c6b0c427362be04c422e9f
Vatsal <vatsalgoyal44@gmail.com>
2023-04-13 15:28:53 +0530

    match page UI

--- client/src/Components/pages/match.js
+++ client/src/Components/pages/match.js
@@ -14,21 +14,41 @@ const Matchpage= (props) => {
     <div class = "gridcontainermatch">
         <div class = "firstcol">
         <div class = "curscore">
-            <div class = "info">
-                <div>
-                    <h3 class="font-bold mx-5 my-1 text-lg">Indian Premier League</h3>
+            <div class = "info border-b-2 border-violet-950">
+                    <h3 class="font-bold mx-3 my-1 text-lg">Indian Premier League</h3>
                     <div class="relative flex">
-                        <a class="mx-5">Date</a>
+                        <a class="mx-3">Date</a>
                         <a>Chennai</a>
                     </div>
                 </div>
-            </div>
             <div class = "status">
+            <div class="flex flex-row">
+                <div class="basis-3/4">
+                    <h3 class="font-bold mx-3 my-1 text-xl">Live</h3>
+                    <div class="flex flex-col">
+                        <div class="basis-1/2 relative flex flex-row">
+                            <h3 class="basis-3/4 font-bold ml-10 my-5 text-xl">Chennai Super Kings</h3>
+                            <h3 class="basis-1/4 font-bold my-5 text-xl">100/8</h3>
+                        </div>
+                        <div class="basis-1/2 relative flex flex-row">
+                            <h3 class="basis-3/4 font-bold ml-10 my-5 text-xl">Mumbai Indians*</h3>
+                            <h3 class="basis-1/4 font-bold my-5 text-xl">100/8</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="basis-1/4 flex flex-col">
+                    <div class="basis-3/4">
 
+                    </div>
+                    <div class="basis-1/4">
+                        
+                    </div>
+                </div>
+            </div>
             </div>
         </div>
-        <div class = "commentarycomp">
-                <div><h3>Commentary</h3></div>
+        <div class = "flex flex-row">
+                <div class="basis-5"><h3 class="font-bold mx-3 my-1 text-lg">Commentary</h3></div>
                 <div class="commentary">
 
                 </div>
