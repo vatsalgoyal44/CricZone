cb47bcfd3c633c6f3787455ea87bb23ab5333c19
Kanishka Mittal <kanishka.mittal279@gmail.com>
2023-04-29 07:22:48 +0530

    merge

--- data_creation/data.sh
+++ data_creation/data.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
 
 # Create an array of team names
-teams=('IND' 'AUS' 'AFG' 'BAN' 'NZ' 'ENG')
+teams=('IND' 'AUS' 'AFG' 'BAN' 'NZL' 'ENG')
 
 # Set the tour name and number of overs
 tour='ICC T20 Worldcup 2023'
 overs=20
 
 # Create an array of famous stadium names
-venues=('Lord\'s' 'MCG' 'Eden Gardens' 'Wankhede Stadium' 'The Oval' 'Old Trafford' 'M. Chinnaswamy Stadium' 'Newlands' 'Lahore Gaddafi Stadium' 'Sharjah Cricket Stadium' 'Dubai International Cricket Stadium' 'Sydney Cricket Ground')
+venues=('MCG' 'Eden Gardens' 'Wankhede Stadium' 'The Oval' 'Old Trafford' 'M. Chinnaswamy Stadium' 'Newlands' 'Lahore Gaddafi Stadium' 'Sharjah Cricket Stadium' 'Dubai International Cricket Stadium' 'Sydney Cricket Ground')
 
-# Loop through all combinations of team pairs
-for ((i=0;i<${#teams[@]};++i)); do
-    for ((j=i+1;j<${#teams[@]};++j)); do
-        # Choose a random venue
-        venue=${venues[$RANDOM % ${#venues[@]}]}
-
-        # Run the simulation command
-        echo "Running simulation: ${teams[i]} vs ${teams[j]} at $venue"
-        python simulate_live.py -t1 ${teams[i]} -t2 ${teams[j]} -tour "$tour" -v "$venue" -o $overs
+# Loop 100 times to play 100 random matches
+for ((i=1;i<=100;++i)); do
+    # Choose two random teams
+    team1=${teams[$RANDOM % ${#teams[@]}]}
+    team2=${teams[$RANDOM % ${#teams[@]}]}
+    
+    # Ensure that the two teams are not the same
+    while [ "$team1" == "$team2" ]; do
+        team2=${teams[$RANDOM % ${#teams[@]}]}
     done
+
+    # Choose a random venue
+    venue=${venues[$RANDOM % ${#venues[@]}]}
+
+    # Run the simulation command
+    echo "Match $i: $team1 vs $team2 at $venue"
+    python simulate_live.py -t1 $team1 -t2 $team2 -tour "$tour" -v "$venue" -o $overs
 done
